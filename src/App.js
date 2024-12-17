c1f6a37567e7a3b625bc6db5eb7bb460cf64bdb6
Wizqqrl <baban1979molotov@gmail.com>
2024-12-18 00:32:48 +0600

    cryptobtns

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import ExchangeForm from './components/ExchangeForm';
+function App() {
+  return (
+    <div className="App">
+      <ExchangeForm/>
+    </div>
+  );
+}
+
+export default App;
